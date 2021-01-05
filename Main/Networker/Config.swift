36af7cab23a7e558c331e5148bd83c472a155dc9
Mojisola Adebiyi <mojisolaadebiyi@gmail.com>
2021-01-05 07:38:27 +0100

    Add config file

---
+++ Main/Networker/Config.swift
@@ -0,0 +1,12 @@
+//
+//  Config.swift
+//  CWDevTest
+//
+//  Created by Mojisola Adebiyi on 27/12/2020.
+//
+
+import Foundation
+
+struct Config {
+    static let apiKey = "1aeaa6c2821c4e71803ce4733d673e69"
+}
